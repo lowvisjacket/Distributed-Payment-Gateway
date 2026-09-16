fe1d33d15737f06eaa8d502429eb69b9557e0d38
lowvisjacket <example@example.com>
2026-09-16 09:47:08 -0500

    Fixed issues with CustomerAccountTests

--- src/test/java/com/example/payment_processor/CustomerAccountTests.java
+++ src/test/java/com/example/payment_processor/CustomerAccountTests.java
@@ -32,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class CustomerAccountTests {
+
     @Autowired
     MockMvc mockMvc;
 
