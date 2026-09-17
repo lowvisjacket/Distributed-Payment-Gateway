09a6396cdd97bfbdd84ee6aa47e0ff49157414d5
lowvisjacket <example@example.com>
2026-09-17 09:46:25 -0500

    Updated CI workflow

--- src/test/java/com/example/payment_processor/CustomerAccountTests.java
+++ src/test/java/com/example/payment_processor/CustomerAccountTests.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(TestcontainersConfiguration.class)
 class CustomerAccountTests {
 
     @Autowired
--- src/test/java/com/example/payment_processor/WalletTests.java
+++ src/test/java/com/example/payment_processor/WalletTests.java
@@ -5,10 +5,12 @@ import com.example.payment_processor.Service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 public class WalletTests {
 
     @Autowired
